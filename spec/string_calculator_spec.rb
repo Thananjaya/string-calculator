f987ea973f60e2c9990ee74cf729b65e1fac2259
thananjaya <thananjaya091993@gmail.com>
2024-08-29 08:46:49 +0530

    added test case for returning zero when empty strings are passed

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,5 +2,12 @@ require "string_calculator"
 
 #spec for string calculator module
 describe StringCalculator do
-  
+  # test case for adding numbers
+  describe ".add" do
+    context "input as empty string" do
+      it "should return zero" do
+        expect(StringCalculator.add(''))
+      end
+    end
+  end
 end
